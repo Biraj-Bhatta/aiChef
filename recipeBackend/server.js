c86c059f7254fee3b8f82a8907f831e5b3c5a14e
Biraj-Bhatta <iambirajbhatta@gmail.com>
2026-06-26 14:56:48 +0545

    fixing issue

--- recipeBackend/server.js
+++ recipeBackend/server.js
@@ -8,22 +8,26 @@ app.use(cors());
 app.use(express.json());
 
 
-app.post("/api/chat", async (req, res) => {
-    console.time("recipe");
-
-    try {
-        const { ingredients } = req.body;
-
-        const generatedRecipe = await getRecipe(ingredients);
-        console.timeEnd("recipe");
-
-
-        res.json(generatedRecipe);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({
-            error: "Server error"
-        });
-    }
+app.get("/", (req, res) => {
+  res.send("Backend Working");
 });
+
+// app.post("/", async (req, res) => {
+//     console.time("recipe");
+//
+//     try {
+//         const { ingredients } = req.body;
+//
+//         const generatedRecipe = await getRecipe(ingredients);
+//         console.timeEnd("recipe");
+//
+//
+//         res.json(generatedRecipe);
+//     } catch (error) {
+//         console.error(error);
+//         res.status(500).json({
+//             error: "Server error"
+//         });
+//     }
+// });
 module.exports = app;
